1d40fc9111fef911573952e2399893cff2c02afb
minyeongg <1030pmy@gmail.com>
2025-04-15 15:29:24 +0900

    First Commit

---
+++ src/main/java/com/site/slowprint/SlowPrintApplication.java
@@ -0,0 +1,13 @@
+package com.site.slowprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowPrintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlowPrintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/site/slowprint/SlowPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.slowprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowPrintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
