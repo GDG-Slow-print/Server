7481ac79630165b927788bf111a3eca3d6c3ded2
Park Seohyun <165402279+xeohyun@users.noreply.github.com>
2025-05-13 03:05:25 +0900

    feat: Domain,dto 구현

--- src/main/java/com/site/slowprint/carbon/domain/Carbon.java
+++ src/main/java/com/site/slowprint/carbon/domain/Carbon.java
@@ -1,4 +1,41 @@
 package com.site.slowprint.carbon.domain;
 
+import com.site.slowprint.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "carbon")
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Carbon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_email", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String transportation;
+
+    @Column(nullable = false)
+    private double distance;
+
+    @Column(nullable = false)
+    private double savedCarbon;
+
+    @Column(nullable = false)
+    private double earnedMileage;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
---
+++ src/main/java/com/site/slowprint/carbon/dto/CarbonRequest.java
@@ -0,0 +1,14 @@
+package com.site.slowprint.carbon.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CarbonRequest {
+
+    private String transportation;
+
+    private double distance;
+
+}
